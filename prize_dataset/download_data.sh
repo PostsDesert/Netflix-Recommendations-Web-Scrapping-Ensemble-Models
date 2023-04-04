1fc9413d51f3ccdcb684a8330b554cde70470162
Griffith Baker <gmbaker3@ncsu.edu>
2023-04-04 13:32:09 -0400

    change download dir

--- prize_dataset/download_data.sh
+++ prize_dataset/download_data.sh
@@ -1,2 +1,2 @@
 # Download the remainder of the prize dataset from Google drive
-gdown https://drive.google.com/drive/folders/1SWSbhXZv1RHP5_MdSXiH_lDS-J6YR1v1 -O ./ --folder
+gdown https://drive.google.com/drive/folders/1SWSbhXZv1RHP5_MdSXiH_lDS-J6YR1v1 -O . --folder
